f8af484574261953592eb1b83a813295890ed2ba
Alex <mwppl123@gmail.com>
2022-12-27 15:58:47 +0100

    Plan and Comments

---
+++ CarsProject/src/carsproject/CarsProject.java
@@ -0,0 +1,17 @@
+//Project to show-off database containing Scirocco's, Lancer's and Volvos c30's
+//Project uses legacy DB from my old Python project
+//Creation date: 27.12.2022
+//Author: 
+package carsproject;
+//Project tasks
+//Using db or csv file
+//Use ifs and queries to properly show selected cars from menu
+//ex. take from user model, price range, mileage range and show deals
+
+public class CarsProject {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
