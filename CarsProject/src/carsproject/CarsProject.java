e28154f5e6eb2e1ddfae10ee39e157088e0a235f
Alex <mwppl123@gmail.com>
2022-12-29 13:38:49 +0100

    Taking users preferences for search

--- CarsProject/src/carsproject/CarsProject.java
+++ CarsProject/src/carsproject/CarsProject.java
@@ -1,17 +1,52 @@
-//Project to show-off database containing Scirocco's, Lancer's and Volvos c30's
-//Project uses legacy DB from my old Python project
-//Creation date: 27.12.2022
-//Author: 
+//Program pracuje na pliku tekstowym w ktorym z wierszach sa dane, pooddzielane przecinkami
+//Dane zostaly skonwertowane z bazy danych w sqlite3 do pliku txt
+//Data powstania: 27.12.2022
+//Autor: 
 package carsproject;
+
+import java.io.*;
+import java.util.Scanner;
 //Project tasks
 //Using db or csv file
 //Use ifs and queries to properly show selected cars from menu
 //ex. take from user model, price range, mileage range and show deals
-
 public class CarsProject {
-
+    
+    public static void readRow(String f, int row){
+        BufferedReader in = null;
+    }
+    public static void readFile(){
+        
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scann = new Scanner(System.in);
+        System.out.print("Wybierz na podstawie jakiego modelu szukasz danych:\n"
+                + "1. Volkswagen Scirocco\n"
+                + "2. Mitsubishi Lancer\n"
+                + "3. Volvo c30\n"
+                + "4. Wszystkie\n"
+                + "0. Wyjdz\n");
+        int model = scann.nextInt();
+        
+        System.out.print("Interesuja mnie modele... \n"
+                + "Jeśli jest to nieistotne prosze wpisać 0\n"
+                + "w zasiegu cenowym \n"
+                + "Od: ");
+        int priceFrom = scann.nextInt();
+        if (priceFrom != 0){
+            System.out.print("Do: ");
+            int priceTo = scann.nextInt();
+        }
+        else{
+            System.out.print("Pomijamy widelki cenowe...");
+        }
+        System.out.print("Z przebiegiem...\n"
+                + "Od: ");
+        int mileageFrom = scann.nextInt();
+        if (mileageFrom != 0){
+            System.out.print("Do: ");
+            int mileageTo = scann.nextInt();
+        }
     }
     
 }
