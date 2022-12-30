96d1cead398acbbdb7d9a2ce5285dca2950611f8
Alex <mwppl123@gmail.com>
2022-12-30 14:46:48 +0100

    Making sure user imput good numbers

--- CarsProject/src/carsproject/CarsProject.java
+++ CarsProject/src/carsproject/CarsProject.java
@@ -13,14 +13,18 @@ import java.util.Scanner;
 //ex. take from user model, price range, mileage range and show deals
 public class CarsProject {
     
+    public static void getResults(String make){
+        
+    }
+    
     public static String[] readRow(String row){
         String elements[] = row.split(",");
-//        String make = elements[0];
-//        String model = elements[1];
-//        int price = Integer.parseInt(elements[2]);
-//        int year = Integer.parseInt(elements[3]);
-//        int mileage = Integer.parseInt(elements[4]);
-        return elements;
+        String make = elements[0];
+        String model = elements[1];
+        int price = Integer.parseInt(elements[2]);
+        int year = Integer.parseInt(elements[3]);
+        int mileage = Integer.parseInt(elements[4]);
+        return 0;
     }
     public static void readFile(String f) throws IOException{
         BufferedReader in = null;
@@ -29,7 +33,7 @@ public class CarsProject {
             in = new BufferedReader(new FileReader(f));
             
             while((row = in.readLine())!= null){
-                String values[] = readRow(row);
+                //String values[] = readRow(row);
             }
         } 
         finally{
@@ -40,34 +44,52 @@ public class CarsProject {
         
     }
     public static void main(String[] args) throws IOException {
+        int inModel = 0, priceFrom, priceTo, mileageFrom, mileageTo;
         Scanner scann = new Scanner(System.in);
         System.out.print("Wybierz na podstawie jakiego modelu szukasz danych:\n"
                 + "1. Volkswagen Scirocco\n"
                 + "2. Mitsubishi Lancer\n"
                 + "3. Volvo c30\n"
-                + "4. Wszystkie\n"
-                + "0. Wyjdz\n");
-        int model = scann.nextInt();
+                + "4. Wszystkie\n");
+        do{
+            System.out.print(">>> ");
+            try{ 
+                inModel = scann.nextInt();
+                if (!(inModel > 0 && inModel < 5)){
+                    System.out.println("Cos sie nie zgadza, sprobuj jeszcze raz \n"
+                            + "Pamietaj, nalezy podac liczbe odpowiadajacej pozycji w menu");
+                }
+            }
+            catch(java.util.InputMismatchException e){
+                System.out.print("Podany zostal znak inny niz liczba\n "
+                        + "Prosze podac liczbe");
+            }
+        }
+        while(!(inModel > 0 && inModel < 5));
+            
         
         System.out.print("Interesuja mnie modele... \n"
                 + "Jeśli jest to nieistotne prosze wpisać 0\n"
                 + "w zasiegu cenowym \n"
                 + "Od: ");
-        int priceFrom = scann.nextInt();
-        if (priceFrom != 0){
-            System.out.print("Do: ");
-            int priceTo = scann.nextInt();
-        }
-        else{
-            System.out.print("Pomijamy widelki cenowe...\n");
-        }
+        do{
+            System.out.print(">>> ");
+            priceFrom = scann.nextInt();
+            if (priceFrom != 0){
+                System.out.print("Do: ");
+                priceTo = scann.nextInt();
+            }
+            else{
+                System.out.print("Pomijamy widelki cenowe...\n");
+            }
+        }while(!(priceFrom >= 0));
         
         System.out.print("Z przebiegiem...\n"
                 + "Od: ");
-        int mileageFrom = scann.nextInt();
+        mileageFrom = scann.nextInt();
         if (mileageFrom != 0){
             System.out.print("Do: ");
-            int mileageTo = scann.nextInt();
+            mileageTo = scann.nextInt();
         }
         readFile("cars.txt");
     }
